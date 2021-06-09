0e7037c358eb42006e6a9b87a7e55fd07e933d54
amin_ruhul <ruhulinfo50@gmail.com>
2021-06-10 00:21:20 +0600

    create card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="max-w-xs rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by John Doe
+        </div>
+
+        <ul>
+          <li>
+            <strong>Views:</strong>4000
+          </li>
+          <li>
+            <strong>Download:</strong>1700
+          </li>
+          <li>
+            <strong>Likes:</strong>500
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag1
+        </span>
+
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag3
+        </span>
+
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag2
+        </span>
+      </div>
     </div>
   );
 }
