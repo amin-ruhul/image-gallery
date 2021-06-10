b7cfeabe0794e71af72f587cabc22fd3c930f868
amin_ruhul <ruhulinfo50@gmail.com>
2021-06-10 20:17:56 +0600

    render data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
+import Card from "./components/Card";
 function App() {
   const [images, setImages] = useState([]);
-  const [isLoading, setIsLoding] = useState(false);
+  const [isLoading, setIsLoding] = useState(true);
   const [term, setTerm] = useState(" ");
 
   useEffect(() => {
@@ -9,43 +10,20 @@ function App() {
       `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoding(false);
+      })
       .catch((err) => console.log(err));
   });
 
   return (
-    <div className="max-w-xs rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by John Doe
-        </div>
-
-        <ul>
-          <li>
-            <strong>Views:</strong>4000
-          </li>
-          <li>
-            <strong>Download:</strong>1700
-          </li>
-          <li>
-            <strong>Likes:</strong>500
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
-
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-      </div>
+    <div className="container mx-auto px-4">
+      {isLoading ? <h1 className='text-6xl text-center ma-auto mt-32'>Loadding...</h1> :<div className="grid grid-cols-4 gap-4">
+        {images.map((image) => (
+          <Card key={image.id} data={image} />
+        ))}
+      </div>}
     </div>
   );
 }
