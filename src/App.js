10cdc7026404988bd5f6c39821a55ca02fbe9c73
amin_ruhul <ruhulinfo50@gmail.com>
2021-06-10 07:50:11 +0600

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import React, { useState, useEffect } from "react";
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoding] = useState(false);
+  const [term, setTerm] = useState(" ");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  });
+
   return (
     <div className="max-w-xs rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full" />
